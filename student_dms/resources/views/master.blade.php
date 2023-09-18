5171c764364747bd16118573f4653d40aa2ca403
sachindu <sachindu@thesanmark.com>
2023-09-18 11:01:24 +0530

    update master.blade.php

--- student_dms/resources/views/master.blade.php
+++ student_dms/resources/views/master.blade.php
@@ -8,24 +8,12 @@
     <title>Student Management System</title>
 </head>
 <body>
-    <div class="card text-center">
-    <div class="card-header">
-        <ul class="nav nav-pills card-header-pills">
-        <li class="nav-item">
-            <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-        </ul>
-    </div>
-    <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="container mt-5">
+        
+        <h1 class="text-primary mt-3 mb-4 text-center"><b>Register a Student</b></h1>
+        
+        @yield('content')
+        
     </div>
 </div>
 </body>
